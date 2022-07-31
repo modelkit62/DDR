e82c459390a10e06e308ee294bb3eb20be780401
modelkit62 <modelkit62@gmail.com>
2022-07-31 22:09:35 +0100

    addition rest

--- src/z2_GameOfLife/GameOfLife.java
+++ src/z2_GameOfLife/GameOfLife.java
@@ -28,6 +28,55 @@ public class GameOfLife {
         System.out.println("========");
     }
 
+    public void setAlive(int x, int y){
+        this.board[x][y] = 1;
+    }
+
+    public int getAliveNeighbours(int x, int y){
+        int count = 0;
+        count += isBorder(x-1, y-1);
+        count += isBorder(x, y-1);
+        count += isBorder(x+1, y-1);
+
+        count += isBorder(x-1, y);
+        count += isBorder(x+1, y);
+
+        count += isBorder(x-1, y+1);
+        count += isBorder(x, y+1);
+        count += isBorder(x+1, y+1);
+
+        return count;
+    }
+
+    private int isBorder(int x, int y) {
+        if(x < 0 || x >= row) return 0;
+        if(y < 0 || y >= column) return 0;
+        return this.board[x][y];
+    }
+
+    private void nextGeneration(){
+        int[][] newBoard = new int[row][column];
+        for(int y = 0; y < row; y++){
+            for (int x = 0; x < column; x++){
+                int aliveNeighbours = getAliveNeighbours(x,y);
+                if(isBorder(x,y) == 1){
+                    if(aliveNeighbours < 2 ){
+                        newBoard[y][x] = 0;
+                    } else if(aliveNeighbours == 2 || aliveNeighbours == 3){
+                        newBoard[y][x] = 1;
+                    } else if(aliveNeighbours > 3){
+                        newBoard[y][x] = 0;
+                    }
+                } else {
+                    if (aliveNeighbours == 3) {
+                        newBoard[x][y] = 1;
+                    }
+                }
+            }
+        }
+        this.board = newBoard;
+    }
+
     public static void main(String[] args) {
         GameOfLife game = new GameOfLife(6, 5);
         game.printBoard();
