0b165f83d8eb8b7f8a83be19c15f7350485b5c53
modelkit62 <modelkit62@gmail.com>
2022-10-04 00:45:42 +0100

    patch routes

---
+++ src/z4_stack/SimpleStack.java
@@ -0,0 +1,12 @@
+package z4_stack;
+
+public interface SimpleStack<T> {
+    /** empty - return true if the stack is empty */
+    abstract boolean empty();
+    /** push - add an element onto the stack */
+    abstract void push(T n);
+    /** pop - return and remove the top element */
+    abstract T pop();
+    /** peek - return the top element but don't remove it */
+    abstract T peek();
+}
